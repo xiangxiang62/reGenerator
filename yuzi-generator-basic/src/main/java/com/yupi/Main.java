2380c4bd0d6a8f485c9aec79f64f03cd892d8838
xiangxiang62 <1964362087@qq.com>
2024-01-07 18:10:12 +0800

    静态/动态文件生成 basic 版

---
+++ yuzi-generator-basic/src/main/java/com/yupi/Main.java
@@ -0,0 +1,18 @@
+package com.yupi;
+
+import com.yupi.generator.StaticGenerator;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        // 获取到文件的根路径
+        String projectPath = System.getProperty("user.dir");
+        //
+        File parentPath = new File(projectPath).getParentFile();
+        // ACM 模板文件
+        String inputPath = new File(parentPath, "yuzi-generator-demo-projects/acm-template").getAbsolutePath();
+        String outputPath = projectPath;
+        StaticGenerator.copyFilesByHutool(inputPath,outputPath);
+    }
+}
